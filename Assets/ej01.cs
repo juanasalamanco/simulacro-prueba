b0c325193306b746885becd384f474aab24b8194
juanasalamanco <juanasalamanco@gmail.com>
2022-04-08 08:21:29 -0300

    Initial commit

---
+++ Assets/ej01.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ej01 : MonoBehaviour
+{
+    public int num1 = 0;
+    public int num2 = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 == num2)
+        {
+            Debug.Log("Ambos numeros son iguales");
+        }
+        else
+        {
+            Debug.Log("Los numeros no son iguales");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
